9f2153c5a074c94b3161d32397d52e3de479d9c1
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-02-10 09:43:33 -0600

    Basic line with RGB triangle test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    p1 = Point(100, 100)
+    p2 = Point(100, 300)
+    p3 = Point(400, 300)
+    line1 = Line(p1, p2)
+    line2 = Line(p2, p3)
+    line3 = Line(p3, p1)
+    win.draw_line(line1, "Red")
+    win.draw_line(line2, "Green")
+    win.draw_line(line3, "Blue")
 
+    win.wait_for_close()
 
 main()
