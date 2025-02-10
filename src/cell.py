edb20226468a9da573397b6f7527d9794ca920d0
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-02-10 11:31:32 -0600

    Resync with course

--- src/cell.py
+++ src/cell.py
@@ -1,30 +1,50 @@
-from line import Line
-from point import Point
+from graphics import Line, Point
+
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
-
-        self.__top_wall = Line(Point(x1, y1), Point(x2, y1))
-        self.__right_wall = Line(Point(x2, y1), Point(x2, y2))
-        self.__botom_wall = Line(Point(x1, y2), Point(x2, y2))
-        self.__left_wall = Line(Point(x1, y1), Point(x1, y2))
-
-        self.has_top_wall = True
-        self.has_right_wall = True
-        self.has_bottom_wall = True
+    def __init__(self, win):
         self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
 
-    def draw(self, canvas, wall_color):
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
         if self.has_top_wall:
-            self.__top_wall.draw(canvas, wall_color)
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
         if self.has_right_wall:
-            self.__right_wall.draw(canvas, wall_color)
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
         if self.has_bottom_wall:
-            self.__botom_wall.draw(canvas, wall_color)
-        if self.has_right_wall:
-            self.__left_wall.draw(canvas, wall_color)
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
 
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
-from window import Window
-from point import Point
-from line import Line
+from graphics import Window
 from cell import Cell
+
 
 def main():
     win = Window(800, 600)
-    # p1 = Point(100, 100)
-    # p2 = Point(100, 300)
-    # p3 = Point(400, 300)
-    # line1 = Line(p1, p2)
-    # line2 = Line(p2, p3)
-    # line3 = Line(p3, p1)
-    # win.draw_line(line1, "Red")
-    # win.draw_line(line2, "Green")
-    # win.draw_line(line3, "Blue")
 
-    c1 = Cell(400, 400, 500, 500)
-    win.draw_cell(c1, "White")
-    c2 = Cell(200, 200, 250, 250)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
     c2.has_bottom_wall = False
-    win.draw_cell(c2, "Green")
+    c2.draw(100, 50, 150, 100)
 
+    c1.draw_move(c2)
 
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     win.wait_for_close()
+
 
 main()
