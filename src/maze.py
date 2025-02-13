dc0bab3dc06a0ccab967adaba570925087e97af5
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-02-13 10:22:03 -0600

    tests. fix col vs row issue

--- src/maze.py
+++ src/maze.py
@@ -10,36 +10,40 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__num_rows = num_rows
-        self.__num_cols = num_cols
-        self.__cell_size_x = cell_size_x
-        self.__cell_size_y = cell_size_y
-        self.__win = win
-        self.__cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
         self._create_cells()
 
     def _create_cells(self):
-        for i in range(0, self.__num_rows):
-            row = []
-            for j in range(0, self.__num_cols):
-                row.append(Cell(self.__win))
-            self.__cells.append(row)
-        for i in range(0, self.__num_rows):
-            for j in range(0, self.__num_cols):
+        for i in range(0, self._num_cols):
+            col_cells = []
+            for j in range(0, self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(0, self._num_cols):
+            for j in range(0, self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        x1 = self.__x1 + j * self.__cell_size_x
-        y1 = self.__y1 + i * self.__cell_size_y
-        x2 = x1 + self.__cell_size_x
-        y2 = y1 + self.__cell_size_y
-        self.__cells[i][j].draw(x1, y1, x2, y2)
+        if self._win is None:
+            return
+        x1 = self._x1 + j * self._cell_size_x
+        y1 = self._y1 + i * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
     def _animate(self):
-        self.__win.redraw()
+        if self._win is None:
+            return
+        self._win.redraw()
         time.sleep(.1)
