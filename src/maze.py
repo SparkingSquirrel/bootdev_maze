6e293a066e16b937f3e839536f51e27965fe6af8
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-02-13 08:37:27 -0600

    create maze cells and draw them (basic)

---
+++ src/maze.py
@@ -0,0 +1,41 @@
+from cell import Cell
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(0, self.__num_rows):
+            print(f'ROW:  {i}')
+            row = []
+            for j in range(0, self.__num_cols):
+                print(f'ROW:  {i}   COL:  {j}')
+                row.append(Cell(self.__win))
+            self.__cells.append(row)
+        for i in range(0, self.__num_rows):
+            for j in range(0, self.__num_cols):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        x1 = self.__x1 + j * self.__cell_size_x
+        y1 = self.__y1 + i * self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
